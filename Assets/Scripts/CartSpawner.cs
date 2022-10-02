2b415b3fb8f2f85e1935de90e58a656d571e8d50
Dylan Li <d426li@uwaterloo.ca>
2022-10-01 20:13:46 -0400

    Added Gameplay Loop and Cart Spawner

--- Assets/Scripts/CartSpawner.cs
+++ Assets/Scripts/CartSpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CartSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject cartPrefab;
+    [SerializeField] float respawnTime = 15f;
+    [SerializeField] float randRange = 5f;
+    public bool shootLeft;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(wildCart());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void spawnCart() {
+        GameObject cart = Instantiate(cartPrefab) as GameObject;
+        cart.transform.position = transform.position;
+        cart.transform.parent = gameObject.transform;
+    }
+
+    IEnumerator wildCart() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime + Random.Range(-randRange, randRange));
+            spawnCart();
+        }
     }
 }
